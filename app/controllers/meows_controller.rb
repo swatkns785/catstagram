dd10c071cc86a07124dcd09f9f18caf7e6437823
Steven Watkins <steven.watkins78@gmail.com>
2015-05-07 22:11:53 -0400

    added change meow button functionality

--- app/controllers/meows_controller.rb
+++ app/controllers/meows_controller.rb
@@ -6,15 +6,33 @@ class MeowsController < ApplicationController
     # Build a Meow by the current user for the post
     meow = current_user.meows.build(post: post)
 
-    if meow.save
-      redirect_to :back, notice: "We heard your Meow!"
-    else
-      redirect_to :back
+    # if meow.save
+    #   redirect_to :back, notice: "We heard your Meow!"
+    # else
+    #   redirect_to :back
+    # end
+
+    respond_to do |format|
+      if meow.save
+        format.html { redirect_to :back, notice: "We heard your Meow!" }
+        format.json { render json: meow }
+      else
+        format.html { redirect_to :back }
+        format.json { render json: meow.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     current_user.meows.destroy(params[:id])
-    redirect_to :back, notice: "All evidence of your meowing has been destroyed!"
+
+    respond_to do |format|
+      format.html do
+        flash[:notice] = "All evidence of your meowing has been destroyed!"
+        redirect_to :back
+      end
+
+      format.json { head :no_content }
+    end
   end
 end
